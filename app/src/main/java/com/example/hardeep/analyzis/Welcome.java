8c6b9a4dd7cfff31f65b9c1368faf8a450aecbfc
Hardeep Singh <hardeep07@yahoo.com>
2016-11-10 22:18:13 -0800

    Intial Commit

---
+++ app/src/main/java/com/example/hardeep/analyzis/Welcome.java
@@ -0,0 +1,27 @@
+package com.example.hardeep.analyzis;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome extends Activity {
+
+    public static final int TIME = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Welcome.this, Main.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME);
+    }
+}
