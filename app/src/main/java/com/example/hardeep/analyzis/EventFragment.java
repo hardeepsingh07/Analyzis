47c80a1bd5554e599e5dc0e7269b4eb3d9fc29e7
Hardeep Singh <hardeep07@yahoo.com>
2016-11-11 16:48:21 -0800

    Fab Buttons

--- app/src/main/java/com/example/hardeep/analyzis/EventFragment.java
+++ app/src/main/java/com/example/hardeep/analyzis/EventFragment.java
@@ -1,18 +1,23 @@
 package com.example.hardeep.analyzis;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class EventFragment extends Fragment {
 
+
+    public View view;
+    public ListView nLV, dLV;
 
     // Required empty public constructor
     public EventFragment() {}
@@ -21,7 +26,12 @@ public class EventFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_event, container, false);
+        view = inflater.inflate(R.layout.fragment_event, container, false);
+        nLV = (ListView) view.findViewById(R.id.ef_notification_listview);
+        dLV = (ListView) view.findViewById(R.id.ef_notification_listview);
+
+
+        return view;
     }
 
 }
--- app/src/main/java/com/example/hardeep/analyzis/SessionFragment.java
+++ app/src/main/java/com/example/hardeep/analyzis/SessionFragment.java
@@ -6,22 +6,25 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.robinhood.spark.SparkView;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class SessionFragment extends Fragment {
 
+    public View view;
+    public ListView nLV, dLV;
 
     // Required empty public constructor
     public SessionFragment() {}
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_session, container, false);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view =  inflater.inflate(R.layout.fragment_session, container, false);
+        nLV = (ListView) view.findViewById(R.id.sf_notification_listview);
+        dLV = (ListView) view.findViewById(R.id.sf_notification_listview);
 
+        return view;
+    }
 }
