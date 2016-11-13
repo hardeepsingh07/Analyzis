450861526c9bfb45e2ec367c9e32d0813147564a
Hardeep Singh <hardeep07@yahoo.com>
2016-11-12 16:41:13 -0800

    Graphs Done

--- app/src/main/java/com/example/hardeep/analyzis/EventFragment.java
+++ app/src/main/java/com/example/hardeep/analyzis/EventFragment.java
@@ -9,6 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +24,7 @@ public class EventFragment extends Fragment {
 
 
     public View view;
+    public PieChart pieChart;
     public ListView nLV, dLV;
 
     // Required empty public constructor
@@ -27,10 +35,40 @@ public class EventFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_event, container, false);
-        nLV = (ListView) view.findViewById(R.id.ef_notification_listview);
-        dLV = (ListView) view.findViewById(R.id.ef_notification_listview);
+        pieChart = (PieChart) view.findViewById(R.id.ef_pie_chart);
+        dLV = (ListView) view.findViewById(R.id.ef_data_listview);
 
+        //Empty Description
+        Description ds = new Description();
+        ds.setText("");
+        pieChart.setDescription(ds);
 
+        //Pie Chart
+        List<PieEntry> entries = new ArrayList<>();
+
+        entries.add(new PieEntry(18.5f, "Screen1"));
+        entries.add(new PieEntry(26.7f, "Screen2"));
+        entries.add(new PieEntry(24.0f, "Screen3"));
+        entries.add(new PieEntry(30.8f, "Screen4"));
+        entries.add(new PieEntry(40.5f, "Screen5"));
+
+        PieDataSet set = new PieDataSet(entries, "");
+        set.setValueTextSize(13f);
+        set.setValueLineColor(getResources().getColor(R.color.colorIcons));
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+        colors.add(getResources().getColor(R.color.colorAccent));
+        colors.add(getResources().getColor(R.color.colorPrimaryDark));
+        colors.add(getResources().getColor(R.color.colorPrimaryLight));
+        colors.add(getResources().getColor(R.color.colorSecondaryText));
+        colors.add(getResources().getColor(R.color.colorDivider));
+        colors.add(ColorTemplate.getHoloBlue());
+        set.setColors(colors);
+
+        PieData data = new PieData(set);
+        pieChart.setData(data);
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.invalidate();
         return view;
     }
 
--- app/src/main/java/com/example/hardeep/analyzis/SessionFragment.java
+++ app/src/main/java/com/example/hardeep/analyzis/SessionFragment.java
@@ -8,22 +8,102 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-import com.robinhood.spark.SparkView;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SessionFragment extends Fragment {
 
     public View view;
+    public LineChart lineChart;
     public ListView nLV, dLV;
 
     // Required empty public constructor
-    public SessionFragment() {}
+    public SessionFragment() {
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view =  inflater.inflate(R.layout.fragment_session, container, false);
-        nLV = (ListView) view.findViewById(R.id.sf_notification_listview);
-        dLV = (ListView) view.findViewById(R.id.sf_notification_listview);
+        view = inflater.inflate(R.layout.fragment_session, container, false);
+        lineChart = (LineChart) view.findViewById(R.id.sf_line_chart);
+        dLV = (ListView) view.findViewById(R.id.sf_data_listview);
+
+        List<Entry> values = new ArrayList<Entry>();
+        values.add(new Entry(01f, 10));
+        values.add(new Entry(02f, 30));
+        values.add(new Entry(03f, 20));
+        values.add(new Entry(04f, 40));
+
+
+        //Each line Data Set
+        LineDataSet lineDataSet = new LineDataSet(values, "Number of Sessions");
+        lineDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
+        lineDataSet.setColor(getResources().getColor(R.color.colorPrimaryDark));
+        lineDataSet.setDrawCircles(true);
+        lineDataSet.setCircleColor(getResources().getColor(R.color.colorPrimary));
+        lineDataSet.setDrawHorizontalHighlightIndicator(true);
+        lineDataSet.setDrawVerticalHighlightIndicator(false);
+        lineDataSet.setDrawFilled(true);
+        lineDataSet.setFillColor(getResources().getColor(R.color.colorAccent));
+        lineDataSet.setLineWidth(1.5f);
+        lineDataSet.setValueTextSize(12.0f);
+        lineDataSet.setMode(LineDataSet.Mode.HORIZONTAL_BEZIER);
+
+        //ArrayList of DataSet
+        List<ILineDataSet> iLineDataSets = new ArrayList<>();
+        iLineDataSets.add(lineDataSet);
+
+
+        //Graph Line Data Set
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.getAxisLeft().setDrawGridLines(false);
+        lineChart.getXAxis().setDrawGridLines(false);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
+
+        //disable right chart
+        YAxis yAxisRight = lineChart.getAxisRight();
+        yAxisRight.setEnabled(false);
+        YAxis yAxisLeft = lineChart.getAxisLeft();
+        yAxisLeft.setEnabled(false);
+
+        // the labels that should be drawn on the XAxis
+        final String[] quarters = new String[]{"Q1", "Q2", "Q3", "Q4"};
+
+        IAxisValueFormatter formatter = new IAxisValueFormatter() {
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                System.out.println(value);
+                return quarters[(int) value - 1];
+            }
+
+            @Override
+            public int getDecimalDigits() {
+                return 0;
+            }
+        };
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
+        xAxis.setValueFormatter(formatter);
+
+        //Empty Description
+        Description ds = new Description();
+        ds.setText("");
+        lineChart.setDescription(ds);
 
         return view;
     }
